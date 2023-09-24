1538a8d5e94b51e984e800547f0bfb2527ef73c1
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-09-24 19:54:40 +0300

    added four different ways on how to reverse a string

--- exercises/1. reversestring/index.js
+++ exercises/1. reversestring/index.js
@@ -6,20 +6,38 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) 
-{
+
+
     // Method 1: using built in methods
+// function reverse(str) 
+// {
 //    return str.split("").reverse().join("")
+// }
+
+ //Method 3: using the latest for-loop syntax
+// function reverse(str) {
+    
+//      let reversedString= "";
+//      for (let character of str) {
+//        reversedString = character + reversedString;             
+//      }
+//      return reversedString;
+// }
 
     //Method 2: using for-loop
-    let reversedString = "";
-    for (let i = str.length - 1; i >= 0; i--){
-        reversedString += str[i];
-    }
-    return reversedString;
+// function reverse(str) 
+// {
+//     let reversedString = "";
+//     for (let i = str.length - 1; i >= 0; i--){
+//         reversedString += str[i];
+//     }
+//     return reversedString;
+// }
 
-
-}
+//Method 4: using reduce method
+function reverse(str) {
+    return str.split('').reduce((rev, char) => char + rev, '');
+} 
 
 module.exports = reverse;
-// console.log(reverse("apple"));
+console.log(reverse("apple"));
